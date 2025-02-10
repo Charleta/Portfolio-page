ecaf16b1f6c4d19ec711ae947c1b31cf49280c9d
charleta <sr.carlosortega@gmail.com>
2025-02-10 19:07:16 -0300

    Add json

--- my-project/app/(routes)/about-me/page.tsx
+++ my-project/app/(routes)/about-me/page.tsx
@@ -16,7 +16,13 @@ const AboutMe = ( ) => {
             
             <Avatar/>
             
-            <h1>Hola</h1>
+            <h1 className="text-2xl landig-tight text-center md:text-left
+             md:text-5xl md:mt-10"> Toda mi {' '}
+
+
+              <span className="font-bold text-oran">Trayectoria Profesional</span>
+            </h1>
+
 
         </ContainerPage>
         
