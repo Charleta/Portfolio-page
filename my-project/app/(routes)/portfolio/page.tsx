66b313af8b47fdadb68e25380494db9350aa8f25
charleta <sr.carlosortega@gmail.com>
2025-02-13 18:30:47 -0300

    Completed page assembly

--- my-project/app/(routes)/portfolio/page.tsx
+++ my-project/app/(routes)/portfolio/page.tsx
@@ -1,8 +1,30 @@
+import ContainerPage from "@/app/Components/container";
+import TransitionPage from "@/app/Components/transition-page";
+import PortfolioBox from "@/app/Components/portfolio-box";
+import { dataPortfolio } from "@/data";
+
 const PortfolioPage = () => {
+
     return (
-        <div>
-        <h1>Portfolio</h1>
+      <ContainerPage>
+        <TransitionPage />
+
+        <div className="flex flex-col justify-center h-full">
+          <h1
+            className="text-2xl landig-tight text-center md:text-left
+             md:text-5xl md:mt-10"
+          >
+            Ultimos trabajos{" "}
+            <span className="font-bold text-orange-500">realizados</span>
+          </h1>
+
+          <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 sm:grid-cols-2 md:grid-cols-4">
+            {dataPortfolio.map((data) => (
+              <PortfolioBox key={data.id} data={data}/>
+            ))}
+          </div>
         </div>
+      </ContainerPage>
     );
     }
 
---
+++ my-project/app/Components/portfolio-box.tsx
@@ -0,0 +1,59 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+interface PortfolioBoxProps {
+   data: {
+    id:number,
+    title:string,
+    image:string,
+    urlGithub:string,
+    urlDemo:string
+}
+}
+
+const PortfolioBox = (props : PortfolioBoxProps) => {
+
+    const { data } = props;
+    const { id, title, image, urlGithub, urlDemo } = data;
+    return (
+      <div className="p-4 border border-teal-50 rounded-xl">
+        <h3 className="text-xl font-bold mb-4">{title}</h3>
+        <Image
+          src={image}
+          alt="Imagen de proyecto"
+          width={200}
+          height={200}
+          className="rounded-2xl w-full md:w-[200px] h-auto"
+        />
+
+        <div className="flex mt-5 gap-5 justify-between">
+          <Link
+            href={urlGithub}
+            target="blank"
+            rel="noopener noreferrer"
+            className="p-2 transiton duration-150 rounded-lg
+            bg-gray-800 text-white hover:bg-gray-700"
+          >
+            Ir al demo
+          </Link>
+          <Link
+            href={urlGithub}
+            target="blank"
+            rel="noopener noreferrer"
+            className="p-2 transiton duration-150 rounded-lg
+            bg-orange-500 text-white hover:bg-gray-700"
+          >
+            Github
+          </Link>
+        </div>
+      </div>
+    );
+
+
+
+};
+
+
+
+export default PortfolioBox;
