434bb998ff2e6159412fdd885f7389d51aa0e614
carlos ortega <sr.carlosortega@gmail.com>
2025-04-21 15:02:31 -0300

    fix

--- my-project/app/layout.tsx
+++ my-project/app/layout.tsx
@@ -1,14 +1,13 @@
 
 
 import type { Metadata } from "next"; // sirve para definir el tipo de dato de la metadata, como el titulo y la descripcion
-import {  Space_Grotesk } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import NavBar from "./Components/navBar";
 import Header from "./Components/header";
 import CoverParticles from "./Components/cover-particles";
 
 const SpaceGrotesk = Space_Grotesk({
-  // aca tenemos una variable que se le asigna una funcion, que es la funcion Urbanist, esta funcion recibe un objeto con las propiedades que se le quieren asignar a la fuente
   subsets: ["latin"],
 });
 
